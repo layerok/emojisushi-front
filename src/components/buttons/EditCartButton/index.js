96a72d1f4b74cd2c49f52057ea0f8b141a500bce
Layerok <kotopes231@gmail.com>
2022-05-22 19:18:00 +0200

    edit cart items button opens cart modal

--- src/components/buttons/EditCartButton/index.js
+++ src/components/buttons/EditCartButton/index.js
@@ -1,11 +1,12 @@
 import {SvgIcon} from "../../svg/SvgIcon";
 import {PencilSvg} from "../../svg/PencilSvg";
 import {UIButton} from "../UIButton";
+import {forwardRef} from "react";
 
-export const EditButton = () => {
-    return <UIButton text={"Отредактировать заказа"}>
+export const EditCartButton = forwardRef((props, ref) => {
+    return <UIButton ref={ref} text={"Отредактировать заказа"}>
         <SvgIcon color={"white"} width={"25px"}>
             <PencilSvg/>
         </SvgIcon>
     </UIButton>
-}
+})
