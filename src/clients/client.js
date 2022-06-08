d6d5a408ec5b8b58eb7c0d64f6dae2333415bb68
Layerok <kotopes231@gmail.com>
2022-06-08 16:19:12 +0200

    send session id as parameter for each api request

--- src/clients/client.js
+++ src/clients/client.js
@@ -1,8 +1,19 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const client = axios.create({
     baseURL: `${process.env.REACT_APP_API_BASE_URL}`,
 });
+
+client.interceptors.request.use((config = {}) => {
+    // send session_id from cookies as parameter for each api request
+    const params = config.params || {};
+    const session_id = Cookies.get('session_id');
+    if(session_id) {
+        params.session_id = session_id;
+    }
+    return config;
+})
 
 export {
     client
