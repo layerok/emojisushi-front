33d9827eacb6a01e770654bf2e1802d70b22f533
Layerok <kotopes231@gmail.com>
2022-06-08 21:52:34 +0200

    remove react strict mode

--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ SessionService.init();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <Provider {...stores}>
-              <App />
-          </Provider>
-      </BrowserRouter>
-  </React.StrictMode>
+    <BrowserRouter>
+        <Provider  {...stores}>
+            <App />
+        </Provider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
