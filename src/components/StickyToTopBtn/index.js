d405dfb18271f67696e50e880a166d2eae44f0ee
Layerok <kotopes231@gmail.com>
2022-06-14 19:52:47 +0200

    fix styles

--- src/components/StickyToTopBtn/index.js
+++ src/components/StickyToTopBtn/index.js
@@ -2,23 +2,35 @@ import {SvgIcon} from "../svg/SvgIcon";
 import {ArrowUpSvg} from "../svg/ArrowUpSvg";
 import {Sticky} from "../Sticky";
 import {useWindowScroll} from "react-use";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding-bottom: 32px;
+
+  @media (max-width: 979px) {
+    transform: scale(0.8);
+    padding-bottom: 0;
+  }
+`
 
 export const StickyToTopBtn = (
 
 ) => {
     const scroll = useWindowScroll();
     return <Sticky show={scroll.y > 500}
-                   bottom={"calc(7% + 130px)"}
+                   bottom={"calc(7% + 100px)"}
                    right={"calc(2% + 31px)"}>
+            <Wrapper>
+                <SvgIcon style={{
+                    cursor: "pointer",
 
-            <SvgIcon style={{
-                cursor: "pointer",
+                }} width={"34px"} onClick={() => {
+                    window.scrollTo(0,0);
+                }}>
+                    <ArrowUpSvg/>
+                </SvgIcon>
+            </Wrapper>
 
-            }} width={"34px"} onClick={() => {
-                window.scrollTo(0,0);
-            }}>
-                <ArrowUpSvg/>
-            </SvgIcon>
 
 
     </Sticky>
