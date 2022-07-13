085320e8b5707c23fe33f3f620b6bef911c972be
Layerok <kotopes231@gmail.com>
2022-07-13 10:08:44 +0200

    store translations in json format

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import {ru} from './locales/ru';
-import {ua} from './locales/ua';
+import ru from './locales/ru.json';
+import ua from './locales/ua.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
