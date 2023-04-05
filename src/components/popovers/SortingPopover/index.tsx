2ff55a362c12d70aa774545b15a8d0e1f351e746
Vladimir <18424848+adjustmentlayer@users.noreply.github.com>
2023-04-05 19:19:26 +0300

    fix sorting popover

--- src/components/popovers/SortingPopover/index.tsx
+++ src/components/popovers/SortingPopover/index.tsx
@@ -1,67 +1,33 @@
 import { DropdownPopover } from "../DropdownPopover";
 import { SortOrderButton } from "../../buttons/SortOrderButton";
-import { Suspense, useContext, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Skeleton from "react-loading-skeleton";
-import {
-  Await,
-  UNSAFE_RouteContext,
-  useAsyncValue,
-  useRouteLoaderData,
-} from "react-router-dom";
-import { loader } from "~pages/Category";
-import { IGetProductsResponse } from "~api/menu.api";
-
-const invariant = (condition, message) => {
-  if (!condition) {
-    throw message;
-  }
-};
-
-const useRouteContext = () => {
-  return useContext(UNSAFE_RouteContext);
-};
-
-const useCurrentRouteId = () => {
-  const route = useRouteContext();
-  const thisRoute = route.matches[route.matches.length - 1];
-  invariant(
-    thisRoute.route.id,
-    `useCurrentRouteId can only be used on routes that contain a unique "id"`
-  );
-  return thisRoute.route.id;
-};
+import { SortKey } from "~api/menu.api.types";
 
 export const SortingPopover = ({
   showSkeleton = false,
 }: {
   showSkeleton?: boolean;
 }) => {
-  const routeId = useCurrentRouteId();
-
-  // todo: pass sort_options as props
-  const { productsQuery } = useRouteLoaderData(routeId) as ReturnType<
-    typeof loader
-  >["data"];
-
   if (showSkeleton) {
     return <Skeleton width={120} height={25} />;
   }
-  return (
-    <Suspense fallback={<Skeleton width={120} height={25} />}>
-      <Await resolve={productsQuery}>
-        <InternalSortingDropdown />
-      </Await>
-    </Suspense>
-  );
+  // todo: fetch this options from backend, becase backend is a source of truth
+  // but for now, it is ok
+  const options: SortKey[] = [
+    "bestseller",
+    "latest",
+    "oldest",
+    "price_high",
+    "price_low",
+    "ratings",
+  ];
+  return <InternalSortingDropdown options={options} />;
 };
 
-const InternalSortingDropdown = () => {
-  const productsQuery = useAsyncValue() as IGetProductsResponse;
-
-  const options = productsQuery.sort_options;
-
-  const initialSelectedIndex = options.indexOf("bestsellar");
+const InternalSortingDropdown = ({ options = [] }: { options?: SortKey[] }) => {
+  const initialSelectedIndex = options.indexOf("bestseller");
 
   const [selectedIndex, setSelectedIndex] = useState(
     initialSelectedIndex === -1 ? 0 : initialSelectedIndex
