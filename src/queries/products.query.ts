e1830e9c96e3f2980496bda45979af43825848fc
layer ok <18424848+adjustmentlayer@users.noreply.github.com>
2023-07-06 00:19:16 +0300

    handle 404 error for products query

--- src/queries/products.query.ts
+++ src/queries/products.query.ts
@@ -1,10 +1,28 @@
 import { menuApi } from "~api";
 import { IGetProductsParams, IGetProductsRes } from "~api/types";
 import { QueryOptions } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 export const productsQuery = (
   params: IGetProductsParams
 ): QueryOptions<IGetProductsRes> => ({
   queryKey: ["products", "list", params ?? "all"],
-  queryFn: () => menuApi.getProducts(params).then((res) => res.data),
+  queryFn: async () => {
+    try {
+      const res = await menuApi.getProducts(params);
+      return res.data;
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        if (e.response.status === 404) {
+          return {
+            data: [],
+            total: 0,
+            sort_options: [],
+            filters: [],
+          };
+        }
+        throw e;
+      }
+    }
+  },
 });
