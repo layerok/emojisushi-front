2e0d5cf734c72f3425b2e4e1baa6374edc685fc8
Vladimir <18424848+adjustmentlayer@users.noreply.github.com>
2023-12-03 05:08:13 +0200

    add util to insert element into the array in immutable way

--- src/utils/arr.utils.ts
+++ src/utils/arr.utils.ts
@@ -9,3 +9,7 @@ export const arrImmutableReplaceAt = (
 export const arrImmutableDeleteAt = (arr: any[], index) => {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
 };
+
+export const arrImmutableInsertAt = (arr: any[], index: number, value: any) => {
+  return [...arr.slice(0, index), value, ...arr.slice(index)];
+};
