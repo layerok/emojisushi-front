39d3409fbe5d0680beff19d921e53dd90651cd16
Vladimir <18424848+adjustmentlayer@users.noreply.github.com>
2024-01-28 20:29:24 +0200

    update working hours

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,9 @@
 export type WorkingHours = [[number, number], [number, number]];
 
 export const appConfig = {
+  // todo: extract working hours to a remote config
   workingHours: [
     [10, 0],
-    [21, 15],
+    [21, 45],
   ] as WorkingHours,
 };
