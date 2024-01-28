9738465e82bdc73e647b442b84f080b8cba4f377
Vladimir <18424848+adjustmentlayer@users.noreply.github.com>
2024-01-28 19:56:06 +0200

    add posibility to specify a hash for cache busting of translations via environment variables

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,27 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpBackend from "i18next-http-backend";
+import HttpBackend, { HttpBackendOptions } from "i18next-http-backend";
 import { appStore } from "~stores/appStore";
+
+const getVersion = () => {
+  // todo: find a way to generate automatically an unique hash per each deployment
+  const version = process.env.REACT_APP_TRANSLATIONS_VERSION;
+  return version ? `?version=${version}` : "";
+};
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(HttpBackend)
   .init({
     lng: "uk",
-    defaultNS: "trans22",
+    defaultNS: "translation",
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
     debug: process.env.NODE_ENV === "development",
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json" + getVersion(),
+    } as HttpBackendOptions,
   });
 
 i18n.on("languageChanged", (lng) => {
