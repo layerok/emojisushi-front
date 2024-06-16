d612fbbe56d1303578dac8cb80c3a27aeb53fee1
layer ok <18424848+adjustmentlayer@users.noreply.github.com>
2024-06-17 01:16:39 +0300

    refactor ProductPage remove unnecessary observer rename filterHiddenCategory to filterProductsFromHiddenCategories destructure searchParams as early as possible

--- src/domains/product/pages/ProductPage/index.tsx
+++ src/domains/product/pages/ProductPage/index.tsx
@@ -11,20 +11,17 @@ import {
 import { IProduct, SortKey } from "src/api/types";
 import { CategorySlug } from "~domains/category/constants";
 import { DefaultErrorBoundary } from "~components/DefaultErrorBoundary";
-import { observer } from "mobx-react";
+
 import { ROUTES } from "~routes";
 import {
   useTypedParams,
   useTypedSearchParams,
 } from "react-router-typesafe-routes/dom";
 
-export const ProductPage = observer(() => {
+export const ProductPage = () => {
   const { categorySlug } = useTypedParams(ROUTES.CATEGORY.SHOW);
 
-  const [searchParams] = useTypedSearchParams(ROUTES.CATEGORY.SHOW);
-  const { q } = searchParams;
-
-  const sort = searchParams.sort as SortKey;
+  const [{ q, sort }] = useTypedSearchParams(ROUTES.CATEGORY.SHOW);
 
   const { data: cart, isLoading: isCartLoading } = useQuery(cartQuery);
   const { data: categoryQueryRes, isLoading: isCategoriesLoading } = useQuery({
@@ -37,7 +34,7 @@ export const ProductPage = observer(() => {
   const { data: productQueryRes, isLoading: isProductsLoading } = useQuery(
     productsQuery({
       category_slug: CategorySlug.Menu,
-      sort: sort,
+      sort: sort as SortKey,
       limit: 9999,
     })
   );
@@ -45,8 +42,8 @@ export const ProductPage = observer(() => {
   const belongsToCategory = (product: IProduct) =>
     !!product.categories.find((category) => category.slug === categorySlug);
 
-  // todo: filter hidden categories on the server
-  const filterHiddenCategories = (product: IProduct) => {
+  // todo: filter products from hidden categories on the server
+  const filterProductsFromHiddenCategories = (product: IProduct) => {
     return !!product.categories.find((category) =>
       (categoryQueryRes?.data || [])
         .map((category) => category.slug)
@@ -55,7 +52,7 @@ export const ProductPage = observer(() => {
   };
 
   const rawItems = (productQueryRes?.data || []).filter(
-    q ? filterHiddenCategories : belongsToCategory
+    q ? filterProductsFromHiddenCategories : belongsToCategory
   );
   const items = fuzzySearch(rawItems, q).map((product) => new Product(product));
 
@@ -79,7 +76,7 @@ export const ProductPage = observer(() => {
       />
     </div>
   );
-});
+};
 
 export const Component = ProductPage;
 
